d4f8b28d76a320f898569d9c167d06f2f26f032c
gsspdev <dev@gssp.io>
2023-12-30 17:41:00 -1000

    renamed to wizard, updated model, fixed development signing

---
+++ Wizard/Wizard.swift
@@ -0,0 +1,26 @@
+//
+//  Wizard.swift
+//  Wizard
+//
+//  Created by gssp on 12/28/23.
+//
+
+import SwiftUI
+
+@main
+struct MacGPTApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .frame(width: 400, height:600)
+        }
+        .commands {
+            }
+//            .windowStyle(.hiddenTitleBar)
+//            .windowToolbarStyle(.unified)
+//            .windowResizability(.automatic)
+    }
+}
+
